eed77d365ce802c89fb893388f67a35fc23e5cc7
ChandanKumar51 <chandankumarsafi51@gmail.com>
2024-08-04 12:14:24 +0530

    Project initial Commit

---
+++ src/main/java/com/ashokit/GitSprinBootAppApplication.java
@@ -0,0 +1,16 @@
+package com.ashokit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class GitSprinBootAppApplication {
+
+	public static void main(String[] args) {
+	ConfigurableApplicationContext run =	SpringApplication.run(GitSprinBootAppApplication.class, args);
+	
+		run.close();
+	}
+
+}
